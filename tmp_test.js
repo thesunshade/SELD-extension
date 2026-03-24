4e3cf0518ba663794bdef55ad389c9f3e7435b27
thesunshade <82448383+thesunshade@users.noreply.github.com>
2026-03-24 14:54:14 +0530

    update icon

--- tmp_test.js
+++
@@ -1,8 +0,0 @@
-import { normalizeSinhala } from './utils/normalization.js';
-
-const q = 'පීරන්';
-const w = 'පීරනවා';
-
-console.log('Query:', q, 'Normalized:', normalizeSinhala(q));
-console.log('Word:', w, 'Normalized:', normalizeSinhala(w));
-console.log('Starts with?', normalizeSinhala(w).startsWith(normalizeSinhala(q)));
