b7cbcb56c14a8ad3ecd8acc9062d08d37696f32e
thesunshade <82448383+thesunshade@users.noreply.github.com>
2026-04-05 07:36:43 +0530

    changes to books, add library favicon

---
+++ assets/books/intro-to-SELD/Synonyms.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Using a named export inside the component or default export? We'll rely on the filename / meta for title in TSX, since it doesn't have frontmatter.
+
+export default function AdvancedUsage() {
+  return (
+    <div>
+      <h2 id="synonyms">Synonyms</h2>
+      <ul>
+        <li><strong>Synonyms should be grammatically equivalent.</strong> That means that in a sentence you should be able to substitute one for the other and the sentence should be grammatically correct.   </li>
+        <li>Synonyms should generally <strong>match in degree</strong>. For example <em>large</em> and <em>gigantic</em> are not synonyms even though they both describe size.   </li>
+        <li>Synonyms <strong>need not match in usage</strong>. For example, <em>father</em> and <em>dad</em> are synonyms even though one is more formal than the other. </li>
+      </ul>
+      <h2 id="compare-relationships">Compare Relationships</h2>
+      <p>Unlike synonyms, compare relationships are completely unstructured. They are useful when the Sinhala meaning of two words is completely different even when the English words may be synonyms. It can also show contrasting words when they are not antonyms. </p>
+
+    </div>
+  );
+}
