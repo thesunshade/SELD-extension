837a17be3c755588bba211085a48e452b551c1e7
thesunshade <82448383+thesunshade@users.noreply.github.com>
2026-04-19 14:19:52 +0530

    change links on test drive page, misc css

--- components/shared/InfoUI.tsx
+++ components/shared/InfoUI.tsx
@@ -6,7 +6,9 @@ export const InfoUI: React.FC = () => {
     return (
         <div className="info-pane blur custom-scroll">
             <h3>About</h3>
-            <p>The Sinhala-English Learner’s Dictionary (SELD) is an experimental draft of a dictionary targeted at English speakers learning Sinhala. Please report errors using the feedback button on each entry.</p>
+            <p>The Sinhala-English Learner’s Dictionary (SELD) is an experimental draft of a dictionary for English speakers learning Sinhala. Report errors using the feedback button.</p>
+            <p>Click words on any web page to look up. You can also use the Dictionary Explorer to browse entries alphabetically.</p>
+            <p>Text to speech provided by Google.</p>
 
             <div className="info-pane-meta">
                 <ul className="info-meta">
@@ -15,12 +17,10 @@ export const InfoUI: React.FC = () => {
                     <li><strong>Entries:</strong> {BUILD_INFO.entryCount.toLocaleString()}</li></ul>
             </div>
 
-            <p>Double click or select words on any web page to look up. You can also use the Dictionary Explorer to browse entries alphabetically.</p>
-            <p>Text to speech provided by Google.</p>
 
-            <h2>
+            <h3>
                 Test drive
-            </h2>
+            </h3>
             <p><a
                 href={browser.runtime.getURL("/explore.html")}
                 onClick={(e) => {
@@ -29,7 +29,7 @@ export const InfoUI: React.FC = () => {
                 }}
             >
                 Here are some pages
-            </a> to try out the extension on.</p>
+            </a> to try out the extension.</p>
         </div>
     );
 };
